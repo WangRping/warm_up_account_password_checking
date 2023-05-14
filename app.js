42905ff7846c3419a149182c1fea0f97c1468131
WangRping <r17369@gmail.com>
2023-05-14 14:36:14 +0800

    add users data

--- app.js
+++ app.js
@@ -2,6 +2,34 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const users = [
+  {
+    firstName: 'Tony',
+    email: 'tony@stark.com',
+    password: 'iamironman'
+  },
+  {
+    firstName: 'Steve',
+    email: 'captain@hotmail.com',
+    password: 'icandothisallday'
+  },
+  {
+    firstName: 'Peter',
+    email: 'peter@parker.com',
+    password: 'enajyram'
+  },
+  {
+    firstName: 'Natasha',
+    email: 'natasha@gamil.com',
+    password: '*parol#@$!'
+  },
+  {
+    firstName: 'Nick',
+    email: 'nick@shield.com',
+    password: 'password'
+  }
+]
+
 app.get('/', (req, res) => {
   res.send(`Welcome`)
 })
