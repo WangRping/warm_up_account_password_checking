dc04779d3af7c1e53762dbeab45d3bdcc72d3f6a
WangRping <r17369@gmail.com>
2023-05-14 16:02:24 +0800

    add checking function in app.js

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 const users = [
   {
@@ -36,6 +39,25 @@ const users = [
 
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.post('/check', (req, res) => {
+  const email = req.body.email
+  const password = req.body.password
+
+  const filteruser = users.filter(user => {
+    return user.email === email && user.password === password
+  })
+
+  if (filteruser.length === 1) {
+    res.send(`Welcome back, ${filteruser[0].firstName}`)
+  } else if (filteruser.length > 1) {
+    console.log(`發生例外狀況,篩選到多個使用者,應進行UsersData偵錯`)
+  } else {
+    const loingError = true
+    res.render('index', { loingError })
+  }
+
 })
 
 app.listen(port, () => {
